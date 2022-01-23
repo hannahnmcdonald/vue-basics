64728be0e7e9b0f44bf01a1cfcb795cb23fa0634
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-22 22:14:38 -0500

    added to do list array to app.vue

--- vue-basics/src/App.vue
+++ vue-basics/src/App.vue
@@ -6,7 +6,12 @@
 export default {
   data() {
     return {
-      title: 'My To Do App'
+      title: 'My To Do App',
+      tasks: [
+        { id: 1, name: 'Learn Vue JS', finished: false },
+        { id: 2, name: 'Build a Vue application', finished: false },
+        { id: 3, name: 'Write an article about Vue JS', finished: false }
+      ]
     }
   }
 }
